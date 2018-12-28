93a1fb41ac7f430245fe5a74f55cdd83fd4c436a
RideSolo <ridesolo@protonmail.com>
2018-12-29 06:57:22 +0800

    Update ColdStaking.sol

--- ColdStaking.sol
+++ ColdStaking.sol
@@ -27,33 +27,32 @@ library SafeMath {
   }
 }
 
-contract ColdStaking {    
+contract ColdStaking {
     using SafeMath for uint;
     event Staking(address addr, uint value, uint amount, uint time);
     event WithdrawStake(address staker, uint amount);
     event Claim(address staker, uint reward);
+    event DonationDeposited(address _address, uint value);
 
     struct Staker {
         uint stake;
         uint reward;        
         uint lastClaim;
         uint lastWeightedBlockReward;
+        uint withdrawal_time;
     }
     
     mapping(address => Staker) public staker;
     
-    uint public staked_amount;
-    uint public weightedBlockReward;
     
+    uint public staked_amount;
+    uint public rewardToDistribute;
+    uint public weightedBlockReward;
     uint public totalClaimedReward;
-    // unclaimed rewards can be redistributed, however they are not in this version. 
-    // it can be done durring marketing compaign were to amount of the reward per block will doubeled for example.
-    uint public totalUnClaimedReward;
-    // same as totalUnClaimedReward, totalTreasuryBalance can follow the same schema
-    uint public totalTreasuryBalance;
     uint public lastTotalReward;
     
-    uint public claim_delay = 30 days;
+    uint public claim_delay = 27 days;
+    uint public max_delay = 365 * 2 days; // 2 years.
 
     modifier only_staker {
         require(staker[msg.sender].stake > 0);
@@ -68,6 +67,7 @@ contract ColdStaking {
     function() public payable {
         start_staking();
     }
+    
     // the proposal allow the staker to stake more clo, at any given time without changing the basic description of the formula 
     function start_staking() public payable {
         require(msg.value > 0);
@@ -75,9 +75,9 @@ contract ColdStaking {
         staking_update(msg.value,true);
         staker_reward_update(); 
         staker[msg.sender].stake = staker[msg.sender].stake.add(msg.value); 
-        staker[msg.sender].lastClaim = now;
+        staker[msg.sender].lastClaim = block.timestamp;
         
-        emit Staking(msg.sender,msg.value,staker[msg.sender].stake,now);
+        emit Staking(msg.sender,msg.value,staker[msg.sender].stake,block.timestamp);
     }
 
 
@@ -88,17 +88,15 @@ contract ColdStaking {
         // the smart contract now is independent from any change of the monetary policy.
         // As highlighted by yuriy77k, msg.value should be also deducted from the total.
         
-        uint _total_sub_ = totalTreasuryBalance.add(staked_amount).add(msg.value);
+        uint _total_sub_ = staked_amount.add(msg.value);
         uint _total_add_ = totalClaimedReward;
+        
         uint newTotalReward = address(this).balance.add(_total_add_).sub(_total_sub_);
         uint intervalReward = newTotalReward - lastTotalReward;
         lastTotalReward = lastTotalReward + intervalReward;
         
-        if (staked_amount!=0) {
-            weightedBlockReward = weightedBlockReward.add(intervalReward.mul(1 ether).div(staked_amount));
-        } else {
-            totalUnClaimedReward = totalUnClaimedReward.add(intervalReward);
-        }
+        if (staked_amount!=0) weightedBlockReward = weightedBlockReward.add(intervalReward.mul(1 ether).div(staked_amount));
+        else rewardToDistribute = rewardToDistribute.add(intervalReward);
         
         if(_sign ) staked_amount = staked_amount.add(_value);
         else staked_amount = staked_amount.sub(_value);
@@ -114,29 +112,27 @@ contract ColdStaking {
 
 
     function withdraw_stake() public only_staker {
-        staking_update(staker[msg.sender].stake,false);
-        staker_reward_update();
         
-        uint _stake = staker[msg.sender].stake;
-        staker[msg.sender].stake = 0;
-        msg.sender.transfer(_stake);
+        if(staker[msg.sender].lastClaim + claim_delay > block.timestamp) {
+            
+            staking_update(staker[msg.sender].stake,false);
+            staker_reward_update();
         
-        if(staker[msg.sender].lastClaim + claim_delay > now) {
-            uint _reward = staker[msg.sender].reward;
-            staker[msg.sender].reward = 0;
-            totalUnClaimedReward = totalUnClaimedReward.add(_reward);
+            uint _stake = staker[msg.sender].stake;
+            staker[msg.sender].stake = 0;
+            msg.sender.transfer(_stake);
         }
         
         emit WithdrawStake(msg.sender,_stake);
     }
 
     function claim() public only_rewarded {
-        if(staker[msg.sender].lastClaim + claim_delay <= now) {
+        if(staker[msg.sender].lastClaim + claim_delay <= block.timestamp) {
             
             staking_update(0,true);
             staker_reward_update();
         
-            staker[msg.sender].lastClaim = now;
+            staker[msg.sender].lastClaim = block.timestamp;
             uint _reward = staker[msg.sender].reward;
             staker[msg.sender].reward = 0;
             msg.sender.transfer(_reward);
@@ -152,7 +148,7 @@ contract ColdStaking {
     
     function staker_info() public view returns(uint256 weight, uint256 init, uint256 actual_block,uint256 _reward)
     {
-        uint _total_sub_ = totalTreasuryBalance.add(staked_amount).add(msg.value);
+        uint _total_sub_ = staked_amount;
         uint _total_add_ = totalClaimedReward;
         uint newTotalReward = address(this).balance.add(_total_add_).sub(_total_sub_);
         uint _intervalReward = newTotalReward - lastTotalReward;
@@ -171,7 +167,17 @@ contract ColdStaking {
         );
     }
     
-    function treasury_deposit() public payable {
-        totalTreasuryBalance = totalTreasuryBalance.add(msg.value);
+    function report_inactivity(address _addr) public only_staker
+    {
+        require(staker[_addr].stake > 0);
+        require(block.timestamp > staker[_addr].lastClaim.add(max_delay));
+        
+        uint _amount = staker[_addr].stake;
+        
+        staked_amount = staked_amount.sub(_amount);
+        rewardToDistribute = rewardToDistribute.add(_amount);
+
+        staker[_addr].stake = 0;
+        _addr.transfer(_amount);
     }
 }
